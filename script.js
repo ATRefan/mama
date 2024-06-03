b8087041ba53530869ef17535dbedaa7a5ae0e60
ATRefan <103622242+ATRefan@users.noreply.github.com>
2024-06-03 15:11:38 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,94 +1,180 @@
-const kum_blended = document.getElementById('kum_blended_graph');
+// variabel global buat fetch data dari BE
+// const data = fetch("http://localhost:5000/{link_dari_BE}", {
+//   method: "POST",
+//   headers: { "Content-Type": "application/json" },
+  // body: JSON.stringify(nama_data_dari_BE)
+//   body: JSON.stringify()
+// })
 
-    new Chart(kum_blended, {
-        type: 'doughnut',
-        data: {
-        // labels: ['Red', 'Blue'],
-        datasets: [{
-            // label: '# of Votes',
-            data: [92, 8],
-            backgroundColor:[
-                'rgb(103, 178, 232)',
-                'rgb(255, 183, 0)'
-            ],
-            hoverOffset:4
-        }]
-        }
-    });
 
-    const kum = document.getElementById('kum_graph');
+//user credential
 
-    new Chart(kum, {
-        type: 'doughnut',
-        data: {
-        // labels: ['Red', 'Blue'],
-        datasets: [{
-            // label: '# of Votes',
-            data: [70, 30],
-            backgroundColor:[
-                'rgb(103, 178, 232)',
-                'rgb(255, 183, 0)'
-            ],
-            hoverOffset:4
-        }]
-        }
-    });
+//username
+let user_name = document.getElementById("name");
+// user_name.innerHTML = data.username;
+user_name.innerHTML = "username";
 
-    const kur = document.getElementById('kur_graph');
 
-    new Chart(kur, {
-        type: 'doughnut',
-        data: {
-        // labels: ['Red', 'Blue'],
-        datasets: [{
-            // label: '# of Votes',
-            data: [14, 86],
-            backgroundColor:[
-                'rgb(66, 230, 245)',
-                'rgb(103, 178, 232)'
-            ],
-            hoverOffset:4
-        }]
-        }
-    });
+//donut graph
 
-    const run_off = document.getElementById('runoff_graph');
+let kredit_percentage = 92;
 
-    new Chart(run_off, {
-        type: 'doughnut',
-        data: {
-        // labels: ['Red', 'Blue'],
-        datasets: [{
-            // label: '# of Votes',
-            data: [9, 91],
-            backgroundColor:[
-                'rgb(66, 230, 245)',
-                'rgb(103, 178, 232)'
-            ],
-            hoverOffset:4
-        }]
-        }
-    });
+const kredit = document.getElementById('kredit_graph');
 
-    
-    const kpi_chart = document.getElementById('kpi_chart');
-
-    new Chart(kpi_chart, {
-      type: 'line',
+  new Chart(kredit, {
+      type: 'doughnut',
       data: {
-        labels: ['Jan', 'Feb', 'Mar', 'May', 'Jun', 'Jul'],
-        datasets: [{
-            data: [12, 19, 20, 26, 43, 50],
-            fill: false,
-            borderWidth: 1,
-            tension: 0.1
-        }]
+      datasets: [{
+          data: [kredit_percentage, 100-kredit_percentage],
+          backgroundColor:[
+              'rgb(103, 178, 232)',
+              'rgb(255, 183, 0)'
+          ],
+          hoverOffset:4
+      }]
+      }
+  });
+
+let kredit_val = document.getElementById('kredit_val');
+kredit_val.innerHTML = "kredit"
+
+let dana_percentage = 14;
+const dana = document.getElementById('dana_graph');
+
+  new Chart(dana, {
+      type: 'doughnut',
+      data: {
+      datasets: [{
+          data: [dana_percentage, 100-dana_percentage],
+          backgroundColor:[
+              'rgb(66, 230, 245)',
+              'rgb(103, 178, 232)'
+          ],
+          hoverOffset:4
+      }]
+      }
+  });
+
+  let dana_val = document.getElementById('dana_val');
+  dana_val.innerHTML = "dana"  
+
+  
+let kum_percentage = 70
+const kum = document.getElementById('kum_graph');
+
+  new Chart(kum, {
+      type: 'doughnut',
+      data: {
+      datasets: [{
+          data: [kum_percentage, 100-kum_percentage],
+          backgroundColor:[
+              'rgb(103, 178, 232)',
+              'rgb(255, 183, 0)'
+          ],
+          hoverOffset:4
+      }]
+      }
+  });
+
+let kum_val = document.getElementById('kum_val');
+kum_val.innerHTML = "kum"
+
+let kur_percentage = 9;
+const kur = document.getElementById('kur_graph');
+
+  new Chart(kur, {
+      type: 'doughnut',
+      data: {
+      datasets: [{
+          data: [kur_percentage, 100-kur_percentage],
+          backgroundColor:[
+              'rgb(66, 230, 245)',
+              'rgb(103, 178, 232)'
+          ],
+          hoverOffset:4
+      }]
+      }
+  });
+
+let kur_val = document.getElementById('kur_val');
+kur_val.innerHTML = "kur"
+
+//leads section
+let kum_blended_person_val = document.querySelector('.kum_blended_person_val');
+kum_blended_person_val.innerHTML = "xx";
+
+let kum_person_val = document.querySelector('.kum_person_val');
+kum_person_val.innerHTML = "xx";
+
+let kur_person_val = document.querySelector('.kur_person_val');
+kur_person_val.innerHTML = "xx";
+
+//debitur section
+let jatuh_tempo_npl_val = document.querySelector('.jatuh_tempo_npl_val');
+jatuh_tempo_npl_val.innerHTML = "xx";
+
+let kol2a_npl_val = document.querySelector('.kol2a_npl_val');
+kol2a_npl_val.innerHTML = "xx";
+
+let npl_val = document.querySelector('.npl_val');
+npl_val.innerHTML = "xx";
+
+let npl_rasio_val = document.querySelector('.npl_rasio_val');
+npl_rasio_val.innerHTML = "xx";
+
+//NPL area overview section
+
+//NPL area overview graph
+const ctx = document.getElementById('area_graph');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Area A', 'Area B', 'Area C'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3],
+        borderWidth: 1
+      }]
       },
       options: {
-        scales: {
-        //   y: {
-        //     beginAtZero: true
-        //   }
+        indexAxis: 'y',
+        elements: {
+          bar: {
+            borderWidth: 2,
+          }
+        },
+        responsive: true,
+        plugins: {
+          legend: {
+            display: false
+          },
+        }
+      },
+    });
+  
+
+//KPI graph section
+
+// KPI graph
+let kpi_chart_label = ['Jan', 'Feb', 'Mar', 'May', 'Jun', 'Jul']
+let kpi_chart_data = [12, 19, 20, 26, 43, 50]
+const kpi_chart = document.getElementById('kpi_chart');
+
+  new Chart(kpi_chart, {
+    type: 'line',
+    data: {
+      labels: kpi_chart_label,
+      datasets: [{
+          data: kpi_chart_data,
+          fill: false,
+          borderWidth: 1,
+          tension: 0.1
+      }]
+      },
+    options: {
+      scales: {
+
         y: {
             display: false
         }
@@ -101,39 +187,21 @@ const kum_blended = document.getElementById('kum_blended_graph');
       }
     })
 
+// upload file overlay
+const overlay = document.querySelector('.overlay_upload');
+const upload_file_box = document.querySelector('.upload_file');
+const upload_overlay = document.querySelector('.navigation_share');
+upload_overlay.addEventListener("click", show_overlay);
 
-    const ctx = document.getElementById('area_graph');
 
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['Area A', 'Area B', 'Area C'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        indexAxis: 'y',
-        // Elements options apply to all of the options unless overridden in a dataset
-        // In this case, we are setting the border of each horizontal bar to be 2px wide
-        elements: {
-          bar: {
-            borderWidth: 2,
-          }
-        },
-        responsive: true,
-        plugins: {
-          legend: {
-            // position: 'right',
-            display: false
-          },
-        //   title: {
-        //     display: true,
-        //     text: 'Chart.js Horizontal Bar Chart'
-        //   }
-        }
-      },
-    });
-  
+function show_overlay(){
+  overlay.style.width='600px';
+  overlay.style.height='100%';
+  upload_file_box.style.position='relative';
+}
+
+const close_overlay = document.querySelector('.close');
+close_overlay.addEventListener("click", function(){
+  overlay.style.width = '0';
+  overlay.style.height = '0';
+})
